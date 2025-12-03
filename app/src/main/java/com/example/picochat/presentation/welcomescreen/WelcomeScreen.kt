51276f18e3bd83c80a1031cf07c64ad3c52de312
mekhondokar <sazzad.hossain.swd@gmail.com>
2025-12-03 17:39:59 +0600

    first commit

---
+++ app/src/main/java/com/example/picochat/presentation/welcomescreen/WelcomeScreen.kt
@@ -0,0 +1,11 @@
+package com.example.picochat.presentation.welcomescreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview(showSystemUi = true)
+fun WelcomeScreen(modifier: Modifier = Modifier) {
+    
+}
